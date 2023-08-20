7fb8461b5e75e152cda67ee8f933f1dbfdca0512
Nursyifa Azizah <nursyifaziza@gmail.com>
2023-08-20 09:10:54 +0800

    Add styling

--- frontend/src/VideoDetail.js
+++ frontend/src/VideoDetail.js
@@ -1,7 +1,16 @@
 import {useParams} from 'react-router-dom';
 import {useFetch} from './hooks/useFetch';
 import {useState, useEffect} from 'react';
-import {Button, Heading, Input, Textarea, VStack } from '@chakra-ui/react'
+import {
+    Box,
+    Button,
+    Flex,
+    Heading,
+    Input,
+    Text,
+    Textarea,
+    VStack
+} from '@chakra-ui/react'
 
 const VideoDetail = () => {
     const {videoId} = useParams();
@@ -70,45 +79,48 @@ const VideoDetail = () => {
     }
 
     return (
-        <div className='video-detail-container'>
-            <iframe
-                width={560}
-                height={315}
-                src={video.url}
-                title='Video Player'
-                allowFullScreen></iframe>
+        <Flex className='video-detail-container'>
+            <Box className='video-section'>
+                <iframe
+                    width={560}
+                    height={315}
+                    src={video.url}
+                    title='Video Player'
+                    allowFullScreen></iframe>
 
-            <div className='comments-section'>
-                <Heading as='h3' size='lg'>Comments</Heading>
-                <ul>
-                    {commentsState.map(comment => (
-                        <li key={comment._id}>
-                            <p>{comment.username}</p>
-                            <p>{comment.text}</p>
-                        </li>
-                    ))}
-                </ul>
+                <Box className='comments-section'>
+                    <Heading as='h3' size='lg'>Comments</Heading>
+                    <Box marginBottom='8' padding='4'>
+                    <ul>
+                        {commentsState.map(comment => (
+                            <li key={comment._id}>
+                                <Text as='b'>📩 {comment.username}</Text>
+                                <p>{comment.text}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    </Box>
 
-                <form onSubmit={handleCommentSubmit}>
-                    <Heading as='h3' size='lg'>Add a Comment</Heading>
-                    <VStack spacing='16px'>
-                        <Input
-                            type='text'
-                            name='username'
-                            value={newComment.username}
-                            onChange={handleCommentChange}
-                            placeholder='Your username'/>
-                        <Textarea
-                            name='text'
-                            value={newComment.text}
-                            onChange={handleCommentChange}
-                            placeholder='Your comment'/>
-                        <Button colorScheme='green' type='submit'>Submit Comment {console.log(newComment)}</Button>
-                    </VStack>
-                </form>
-            </div>
-
-            <div className='product-list'>
+                    <form onSubmit={handleCommentSubmit}>
+                        <Heading as='h3' size='lg'>Add a Comment</Heading>
+                        <VStack spacing='16px' alignItems= 'start'>
+                            <Input
+                                type='text'
+                                name='username'
+                                value={newComment.username}
+                                onChange={handleCommentChange}
+                                placeholder='Your username'/>
+                            <Textarea
+                                name='text'
+                                value={newComment.text}
+                                onChange={handleCommentChange}
+                                placeholder='Your comment'/>
+                            <Button colorScheme='green' type='submit'>Submit Comment {console.log(newComment)}</Button>
+                        </VStack>
+                    </form>
+                </Box>
+            </Box>
+            <Box className='product-list' p='16'>
                 <Heading as='h3' size='lg'>Product List</Heading>
                 <ul>
                     {productList.map(product => (
@@ -118,8 +130,8 @@ const VideoDetail = () => {
                         </li>
                     ))}
                 </ul>
-            </div>
-        </div>
+            </Box>
+        </Flex>
     );
 };
 
