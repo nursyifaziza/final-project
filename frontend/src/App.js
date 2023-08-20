d1204e08f92f59e103397d01a4717b7d2e2bb134
Nursyifa Azizah <nursyifaziza@gmail.com>
2023-08-20 08:36:55 +0800

    Install Inter font family

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import {ChakraProvider} from '@chakra-ui/react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Home';
 import VideoDetail from './VideoDetail';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/800.css';
+import theme from './styles/theme'
 
 function App() {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <Router>
                 <Routes>
                     <Route exact path="/" element={< Home />}/>
--- frontend/src/VideoThumbnail.js
+++ frontend/src/VideoThumbnail.js
@@ -5,7 +5,7 @@ import {AspectRatio, Image} from '@chakra-ui/react'
 const VideoThumbnail = ({video}) => {
     return (
         <Link to={`/video/${video._id}`} className='video-thumbnail'>
-            <AspectRatio maxW='400px' ratio={4 / 3}>
+            <AspectRatio maxW='400px' ratio={9 / 16}>
                 <Image src={video.thumbnail} alt={video.title}/>
             </AspectRatio>
             <p>{video.title}</p>
